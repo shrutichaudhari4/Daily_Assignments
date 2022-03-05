e4c9270fa2ddbb331c70e5b962d6fae5b5a8bd47
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-03-05 10:09:10 +0530

    Assignment 2 Completed

---
+++ Assignment_2.py
@@ -0,0 +1,18 @@
+# To Check Whether number is odd or even: 
+# Two Ways:
+
+# 1.Using Function
+def check(n):
+    if n%2==0:
+        return "even"
+    else:
+        return "odd"
+
+print(check(9))
+print(check(8))
+
+# 2.Using Ternary Operator
+a=int(input("Enter a: "))
+check_o="even" if a%2==0 else "odd"
+print(check_o)
+
