33086bb44f006887609bb7051d90656cff7c14e2
Shruti Pralhad Chaudhari <84343751+shrutichaudhari4@users.noreply.github.com>
2022-03-29 23:24:20 +0530

    Area of Circle

---
+++ Assignment_35.py
@@ -0,0 +1,14 @@
+# Area of Circle:
+
+import math
+
+# Using Formula
+rad=int(input("Enter radius of a circle: "))
+pi=3.14
+Area=pi*rad*rad
+print(Area)
+
+# Using import
+r=int(input("Enter r: "))
+print((math.pi)*r**2)
+# print(math.pi)
